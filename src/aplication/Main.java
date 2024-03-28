8f33095e73dd7f3b289269240ef988c4d91bf218
LRJonas <jonas.robertinho@gmail.com>
2024-03-28 15:36:40 -0300

    Position made

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,10 +1,11 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("teste");
-        System.out.println("teste pra ter certeza");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
